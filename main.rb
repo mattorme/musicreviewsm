fb7b5a22f2074b99c079dd03ac78a357c5cf176d
Matt <ormematthewj@gmail.com>
2020-12-03 21:31:31 +1100

    working version, needs css

--- main.rb
+++ main.rb
@@ -3,7 +3,6 @@ require 'sinatra/reloader' if development?
 require 'pg'
 require 'bcrypt'
 require_relative 'db/data_access'
-require 'pry'
 also_reload 'db/data_access' if development?
 
 enable :sessions
